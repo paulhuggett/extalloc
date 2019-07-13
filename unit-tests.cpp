dbf208591ae844bd32872f2f7fc2b36fac3aab7c
Paul Bowen-Huggett <paulhuggett@mac.com>
2019-07-13 10:59:48 +0200

    Additional unit test coverage.

--- unit-tests.cpp
+++ unit-tests.cpp
@@ -1,6 +1,22 @@
 #include "allocator.hpp"
 #include <gtest/gtest.h>
 
+TEST (Allocator, InitialState) {
+    allocator a;
+    EXPECT_EQ (a.num_allocs (), 0U);
+    EXPECT_EQ (a.num_frees (), 0U);
+}
+
+TEST (Allocator, SimpleAllocateThenFree) {
+    allocator a;
+    auto p1 = a.allocate (16);
+    EXPECT_EQ (a.num_allocs (), 1U);
+    EXPECT_EQ (a.num_frees (), 0U);
+    a.free (p1);
+    EXPECT_EQ (a.num_allocs (), 0U);
+    EXPECT_EQ (a.num_frees (), 1U);
+}
+
 TEST (Allocator, AllocFreeAlloc) {
     allocator a;
     auto p1 = a.allocate (16);
@@ -8,8 +24,8 @@ TEST (Allocator, AllocFreeAlloc) {
     auto p2 = a.allocate (16);
     a.free (p2);
     EXPECT_EQ (p1, p2);
-    EXPECT_EQ (a.num_allocs (), 0);
-    EXPECT_EQ (a.num_frees (), 1);
+    EXPECT_EQ (a.num_allocs (), 0U);
+    EXPECT_EQ (a.num_frees (), 1U);
 }
 
 TEST (Allocator, AllocFreeAllocLarger) {
@@ -19,18 +35,23 @@ TEST (Allocator, AllocFreeAllocLarger) {
     auto p2 = a.allocate (32);
     a.free (p2);
     EXPECT_NE (p1, p2);
-    EXPECT_EQ (a.num_allocs (), 0);
-    EXPECT_EQ (a.num_frees (), 1);
+    EXPECT_EQ (a.num_allocs (), 0U);
+    EXPECT_EQ (a.num_frees (), 1U);
 }
 
-TEST (Allocator, Free1) {
+TEST (Allocator, SplitFreeSpace) {
     allocator a;
-    auto p1 = a.allocate (16);
-    EXPECT_EQ (a.num_allocs (), 1);
-    EXPECT_EQ (a.num_frees (), 0);
+    auto p1 = a.allocate (32);
+    auto p2 = a.allocate (16);
     a.free (p1);
-    EXPECT_EQ (a.num_allocs (), 0);
-    EXPECT_EQ (a.num_frees (), 1);
+    auto p3 = a.allocate (16);
+    EXPECT_EQ (p3, p1);
+    EXPECT_EQ (a.num_allocs (), 2U);
+    EXPECT_EQ (a.num_frees (), 1U);
+    a.free (p2);
+    a.free (p3);
+    EXPECT_EQ (a.num_allocs (), 0U);
+    EXPECT_EQ (a.num_frees (), 1U);
 }
 
 TEST (Allocator, Free2) {
@@ -38,39 +59,57 @@ TEST (Allocator, Free2) {
     auto p1 = a.allocate (16);
     auto p2 = a.allocate (16);
     auto p3 = a.allocate (16);
-    EXPECT_EQ (a.num_allocs (), 3);
-    EXPECT_EQ (a.num_frees (), 0);
+    EXPECT_EQ (a.num_allocs (), 3U);
+    EXPECT_EQ (a.num_frees (), 0U);
     a.free (p2);
     a.free (p3);
-    EXPECT_EQ (a.num_allocs (), 1);
-    EXPECT_EQ (a.num_frees (), 1);
+    EXPECT_EQ (a.num_allocs (), 1U);
+    EXPECT_EQ (a.num_frees (), 1U);
     a.free (p1);
-    EXPECT_EQ (a.num_allocs (), 0);
-    EXPECT_EQ (a.num_frees (), 1);
+    EXPECT_EQ (a.num_allocs (), 0U);
+    EXPECT_EQ (a.num_frees (), 1U);
 }
 
-TEST (Allocator, Free3) {
+TEST (Allocator, FreeInReverseOrder) {
     allocator a;
 
     auto p1 = a.allocate (16);
-    EXPECT_EQ (a.num_allocs (), 1);
-    EXPECT_EQ (a.num_frees (), 0);
     auto p2 = a.allocate (16);
-    EXPECT_EQ (a.num_allocs (), 2);
-    EXPECT_EQ (a.num_frees (), 0);
     auto p3 = a.allocate (16);
-    EXPECT_EQ (a.num_allocs (), 3);
-    EXPECT_EQ (a.num_frees (), 0);
+
+    a.free (p3);
+    EXPECT_EQ (a.num_allocs (), 2U);
+    EXPECT_EQ (a.num_frees (), 1U);
+
+    a.free (p2);
+    EXPECT_EQ (a.num_allocs (), 1U);
+    EXPECT_EQ (a.num_frees (), 1U);
 
     a.free (p1);
-    EXPECT_EQ (a.num_allocs (), 2);
-    EXPECT_EQ (a.num_frees (), 1);
-    a.free (p3);
-    EXPECT_EQ (a.num_allocs (), 1);
-    EXPECT_EQ (a.num_frees (), 2);
-    a.free (p2);
-    EXPECT_EQ (a.num_allocs (), 0);
-    EXPECT_EQ (a.num_frees (), 1);
+    EXPECT_EQ (a.num_allocs (), 0U);
+    EXPECT_EQ (a.num_frees (), 1U);
 }
 
+TEST (Allocator, FreeInForwardOrder) {
+    allocator a;
 
+    auto p1 = a.allocate (16);
+    EXPECT_EQ (a.num_allocs (), 1U);
+    EXPECT_EQ (a.num_frees (), 0U);
+    auto p2 = a.allocate (16);
+    EXPECT_EQ (a.num_allocs (), 2U);
+    EXPECT_EQ (a.num_frees (), 0U);
+    auto p3 = a.allocate (16);
+    EXPECT_EQ (a.num_allocs (), 3U);
+    EXPECT_EQ (a.num_frees (), 0U);
+
+    a.free (p1);
+    EXPECT_EQ (a.num_allocs (), 2U);
+    EXPECT_EQ (a.num_frees (), 1U);
+    a.free (p3);
+    EXPECT_EQ (a.num_allocs (), 1U);
+    EXPECT_EQ (a.num_frees (), 2U);
+    a.free (p2);
+    EXPECT_EQ (a.num_allocs (), 0U);
+    EXPECT_EQ (a.num_frees (), 1U);
+}
